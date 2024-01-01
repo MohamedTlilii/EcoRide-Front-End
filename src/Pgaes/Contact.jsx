731db432ac49b27ffd384151690a40d456894c91
NAUT5 <Mohamedtlili1995@hotmail.com>
2024-01-02 00:01:32 +0100

    almost-done

--- src/Pgaes/Contact.jsx
+++ src/Pgaes/Contact.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import Slides from "../Slides/Slides"
+import React from "react";
+import LandingPageFooter from "../Components/LandingPageFooter";
+// import Slides from "../Slides/Slides";
 function Contact() {
   return (
-    <div> <Slides/>  </div>
-  )
+    <div className="contact-parent">
+      <div className="contact-section"></div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
